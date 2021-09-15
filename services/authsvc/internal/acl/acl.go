abd503514d6932dc92e7c440cc14aa50974f122d
Alessandro De Blasis <alex@deblasis.net>
2021-09-15 10:39:35 +0100

    - GetUser By ID - Empty return bugfix on AuthDBSvc
    Fixes #29,  Fixes #30
    
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

--- services/authsvc/internal/acl/acl.go
+++ services/authsvc/internal/acl/acl.go
@@ -5,7 +5,6 @@ import (
 
 	"deblasis.net/space-traffic-control/common/auth"
 	"deblasis.net/space-traffic-control/common/consts"
-	pb "deblasis.net/space-traffic-control/gen/proto/go/authsvc/v1"
 	"github.com/go-kit/kit/log"
 	"github.com/golang-jwt/jwt"
 )
@@ -13,44 +12,21 @@ import (
 func AclRules() map[string]auth.ACLRule {
 
 	return map[string]auth.ACLRule{
-		"/deblasis.v1.AuthService/Signup": func(req interface{}, logger log.Logger) auth.ACLDescriptor {
+		"/deblasis.v1.CentralCommandService/RegisterShip": func(req interface{}, logger log.Logger) auth.ACLDescriptor {
 
-			request := req.(*pb.SignupRequest)
-			if request.Role == consts.ROLE_SHIP {
-				return auth.NewAllGoodAclDescriptor()
-			}
 			return auth.NewMustCheckTokenDescriptor(func(t *jwt.Token) error {
 
 				claims := t.Claims.(jwt.MapClaims) //auth.STCClaims
 				role := claims["role"]
 
-				if role == consts.ROLE_COMMAND {
-					return nil
+				if role != consts.ROLE_SHIP {
+					return fmt.Errorf("unauthorized: you must be a %v in order to perform this operation", consts.ROLE_SHIP)
 				}
-				return fmt.Errorf("unauthorized: you must be a member of %v to perform this operation", consts.ROLE_COMMAND)
 
+				//check if already registered
+
+				return nil
 			})
 		},
 	}
 }
-
-// func(req interface{}) auth.TokenValidator {
-// 	request := req.(*pb.SignupRequest)
-
-// 	if request.Role == consts.ROLE_SHIP {
-// 		return struct{
-// 			allGood
-// 		}
-// 	}
-
-// 	return func(t *jwt.Token) error {
-
-// 		claims := t.Claims.(auth.STCClaims)
-// 		role := claims.Role
-
-// 		if request.Role == consts.ROLE_SHIP || role == consts.ROLE_COMMAND {
-// 			return nil
-// 		}
-// 		return fmt.Errorf("unauthorized: you must be a member of %v to perform this operation", consts.ROLE_COMMAND)
-// 	}
-// },
