a88d0868c17bbce3f69e3ef3c2c103e8574786b1
Alessandro De Blasis <alex@deblasis.net>
2021-09-27 13:50:37 +0100

    LICENSES (#55)
    * LICENSES
    
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>
    
    * fix job
    
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>
    
    * fix job
    
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>
    
    * Licenses
    
    Fixes #54
    
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>
    
    * GH job name
    
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

--- services/clessidrasvc/scripts/consul-agent.sh
+++ services/clessidrasvc/scripts/consul-agent.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 Alessandro De Blasis <alex@deblasis.net>  
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE. 
+#
+
 
 IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 consul agent -bind=$IP -advertise=$IP -join=consul -node=$CONSUL_NODE -dns-port=53 -data-dir=/data -config-file=/consul/service.json -enable-local-script-checks
