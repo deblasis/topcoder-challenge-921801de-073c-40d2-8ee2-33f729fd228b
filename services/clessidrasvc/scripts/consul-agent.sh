42dc4bd0cc5dfc67564ca2200b7ae8b473a30d1f
Alessandro De Blasis <alex@deblasis.net>
2021-09-28 16:11:03 +0100

    docker-compose networks Fixes #64
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

--- services/clessidrasvc/scripts/consul-agent.sh
+++ services/clessidrasvc/scripts/consul-agent.sh
@@ -23,5 +23,5 @@
 #
 
 
-IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+IP=$(ifconfig eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 consul agent -bind=$IP -advertise=$IP -join=consul -node=$CONSUL_NODE -dns-port=53 -data-dir=/data -config-file=/consul/service.json -enable-local-script-checks
